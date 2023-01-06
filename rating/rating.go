f9a60238a10f908dc137326130c5b79139de9ff4
aryan <aryanpal692@gmail.com>
2023-01-06 12:36:52 +0530

    Average Calculation

--- rating/rating.go
+++ rating/rating.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-co-op/gocron"
 )
 
+// here er can create a function for Calculate the Monthly average rating of employee
 func Is_Rating() {
 
 	var users = []entity.User{}
@@ -20,32 +21,44 @@ func Is_Rating() {
 	n := 4
 	// here we use for loop for in users
 	for _, i := range users {
-		// here we can compare a email in task table.
+		// here we can fetch the email in task table.
 		database.Database.Where("assign = ?", i.Email).Find(&task)
-		fmt.Println(i.Email)
 		sum := 0.0
-		for _, k := range task {
-			if k.Rating != 0.0 {
-				c := k.Rating
-				sum = sum + c
+		// here we can compare the role.
+		if i.Role == "employee" {
+			fmt.Println(i.Email)
+			for _, k := range task {
+				if k.Rating != 0.0 {
+					c := k.Rating
+					sum = sum + c
+				}
 			}
-		}
 
-		avg := (sum) / (float64(n))
-		fmt.Println("Sum = ", sum, "\nAverage = ", avg)
+			avg := (sum) / (float64(n))
+			fmt.Println("Sum = ", sum, "\nAverage = ", avg)
+		}
 	}
 }
 
+// here caan create a hello function
 func hello(name string) {
 	message := fmt.Sprintf("Hi, %v", name)
 	fmt.Println(message)
 }
 func RunCronJobs() {
-	s := gocron.NewScheduler(time.UTC)
-	s.Every(1).Seconds().Do(func() {
+	// here we can create a cron job scheduler.
+	s := gocron.NewScheduler(time.Local)
+	// here we can set the time for is rating function.
+	job, _ := s.Every(1).Day().At("12:15").Do(func() {
 		// hello("chonu")
 		Is_Rating()
 
 	})
-	s.StartBlocking()
+	// here we can set the time for hello function.
+	job1, _ := s.Every(1).Day().At("12:17").Do(func() {
+		hello("chonu")
+	})
+	s.StartAsync()
+	fmt.Println(job.ScheduledTime())
+	fmt.Println(job1.ScheduledTime())
 }
